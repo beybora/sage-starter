a6dc21a8cf2377c14ed5a8581cf24307661b3f38
beybora <bora.b.sarikaya@gmail.com>
2025-04-10 21:24:09 +0200

    feat(blocks): add cards-grid custom block

--- wp-content/plugins/custom-gutenberg-blocks/src/cards-grid/render.php
+++ wp-content/plugins/custom-gutenberg-blocks/src/cards-grid/render.php
@@ -1,6 +1,11 @@
 <?php
 
+/**
+ * Server-side rendering for Cards Grid block.
+ * Passes all attributes and content to Blade view.
+ */
+
 echo \Roots\view('sections.cards-grid', [
   'attributes' => $attributes ?? [],
-  'content' => $content ?? '',
+  'content'    => $content ?? '',
 ])->render();
