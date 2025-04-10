e20f68e2137f756f9c238d108914b69eb163ff8d
beybora <bora.b.sarikaya@gmail.com>
2025-04-10 21:52:03 +0200

    refactor: wrap a single post with layout container

--- wp-content/themes/sage-starter-theme/resources/views/single.blade.php
+++ wp-content/themes/sage-starter-theme/resources/views/single.blade.php
@@ -1,7 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-  @while(have_posts()) @php(the_post())
-    @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
-  @endwhile
+    @while (have_posts())
+        @php(the_post())
+        <section class="py-20 bg-white">
+            <div class="max-w-screen-lg mx-auto px-4 sm:px-6 lg:px-8 ">
+                @includeFirst(['partials.content-single-' . get_post_type(), 'partials.content-single'])
+            </div>
+        </section>
+    @endwhile
 @endsection
