af7ef8a7beaf184fce99bad92497027eed9cda19
beybora <bora.b.sarikaya@gmail.com>
2025-04-15 22:19:21 +0200

    feat: configurate tailwind

---
+++ wp-content/themes/sage-starter-theme/tailwind.config.js
@@ -0,0 +1,15 @@
+// filepath: /Users/borabey/Local Sites/Serious Projects/sage-starter-theme/wp-content/themes/sage-starter-theme/tailwind.config.js
+export default {
+  content: [
+    './app/**/*.php',
+    './resources/**/*.{php,js,scss}',
+    './index.php',
+    './functions.php',
+    './single.php',
+    './404.php',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
